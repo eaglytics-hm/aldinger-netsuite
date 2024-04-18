c6985bcd9b1046e50cba8cf37209ca6fbc3afd5d
HM <hieumdd@gmail.com>
2024-04-19 00:36:02 +0700

    feat: Update schema

--- cloud-run/src/pipeline/pipeline.service.ts
+++ cloud-run/src/pipeline/pipeline.service.ts
@@ -11,16 +11,25 @@ export const loadFromGCS = async (filename: string) => {
     const loadOptions = {
         table: tableName,
         schema: [
-            { name: 'id', type: 'NUMERIC' },
-            { name: 'customer', type: 'STRING' },
-            { name: 'lead_start_date', type: 'DATE' },
-            { name: 'project_status', type: 'STRING' },
-            { name: 'revenue', type: 'NUMERIC' },
-            { name: 'sales_vendor', type: 'STRING' },
-            { name: 'setter', type: 'STRING' },
-            { name: 'system_size', type: 'NUMERIC' },
+            { name: 'account', type: 'STRING' },
+            { name: 'business_unit', type: 'STRING' },
+            { name: 'customer_id', type: 'INT64' },
+            { name: 'customer_industry', type: 'STRING' },
+            { name: 'customer_legacy_id', type: 'INT64' },
+            { name: 'customer_name', type: 'STRING' },
+            { name: 'customer_territory', type: 'STRING' },
+            { name: 'date', type: 'DATE' },
+            { name: 'document_number', type: 'STRING' },
+            { name: 'invoice_line', type: 'INT64' },
+            { name: 'item_description', type: 'STRING' },
+            { name: 'item_display_name', type: 'STRING' },
+            { name: 'item_name', type: 'STRING' },
+            { name: 'item_rate', type: 'STRING' },
+            { name: 'location_name', type: 'STRING' },
+            { name: 'quantity', type: 'STRING' },
+            { name: 'shipping_zip', type: 'STRING' },
         ],
     };
 
-    return await load(loadOptions, getFile(filename)).then(() => ({filename}));
+    return await load(loadOptions, getFile(filename)).then(() => ({ filename }));
 };
