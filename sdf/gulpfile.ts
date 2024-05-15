894a1d754561a4194a8c08dc5fb7d881e3a9eaa6
HM <hieumdd@gmail.com>
2024-05-15 21:27:23 +0700

    misc: Add upload to gulpfile

--- sdf/gulpfile.ts
+++ sdf/gulpfile.ts
@@ -1,21 +1,40 @@
+import { join } from 'node:path';
+
+import execa from 'execa';
+import { globSync } from 'glob';
 import gulp, { TaskFunction } from 'gulp';
 import ts from 'gulp-typescript';
 import { rimrafSync } from 'rimraf';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
-const dest = `./src/FileCabinet/SuiteScripts`;
+const scriptPath = './src/FileCabinet/SuiteScripts';
+const scriptDest = () => gulp.dest(scriptPath);
 
 export const clean: TaskFunction = (done) => {
-    rimrafSync(`${dest}/*`, { glob: { ignore: '.gitignore' } });
+    rimrafSync(`${scriptPath}/*.*`, { glob: { ignore: '.gitignore' } });
     done();
+};
+
+export const copy: TaskFunction = () => {
+    return gulp.src('./public/**/*.*').pipe(scriptDest());
 };
 
 export const build: TaskFunction = () => {
     const project = ts.createProject('tsconfig.json');
-    return gulp.src('./src/TypeScript/**/!(*.d).ts').pipe(project()).pipe(gulp.dest(dest));
+    return gulp.src('./src/TypeScript/**/*.ts').pipe(project()).pipe(scriptDest());
 };
 
-export const copy: TaskFunction = () => {
-    return gulp.src('./public/**/*.*').pipe(gulp.dest(dest));
+export const upload: TaskFunction = (done) => {
+    const { file } = yargs(hideBin(process.argv))
+        .options({ file: { type: 'string' } })
+        .parseSync();
+    const fileList = globSync(file ? `**/${file}.js` : '**/*.js', { cwd: join(scriptPath, '..') });
+    execa.sync('suitecloud', ['file:upload', '--paths', ...fileList.map((path) => join('/', path))], {
+        stdout: process.stdout,
+        stderr: process.stderr,
+    });
+    done();
 };
 
-export default gulp.series(clean, gulp.parallel(build, copy));
+export default gulp.series(clean, gulp.parallel(build, copy), upload);
