e12d907d2ea20dc2488c93645400a5b8d6c969e7
HM <hieumdd@gmail.com>
2024-07-11 20:32:47 +0700

    refactor: Refactor getConfig

--- sdf/src/TypeScript/com.eaglytics/ss-extract.mr.ts
+++ sdf/src/TypeScript/com.eaglytics/ss-extract.mr.ts
@@ -57,107 +57,119 @@ const upload = (rows: any[]) => {
     return [null, filename] as const;
 };
 
-const configs = {
-    customdeploy_eag_ss_extract_1772: {
-        id: 1772,
-        parse: (values: any) => ({
-            account: <string>values.account?.text,
-            amount: <number>parseFloat(values['amount']),
-            business_unit: <string>values.class?.text,
-            customer_id: <string>values['entityid.customer'],
-            customer_industry: <string>values['custentity_esc_industry.customer']?.text,
-            customer_legacy_id: <string>values['custentity_ald_legacy_id.customer'],
-            customer_name: <string>values['altname.customer'],
-            customer_territory: <string>values['territory.customer']?.text,
-            date: (() => {
-                const strValue = <string>values.trandate;
-                const date = dayjs(strValue);
-                return date.isValid() ? date.format('YYYY-MM-DD') : null;
-            })(),
-            document_number: <string>values.tranid,
-            invoice_line: <number>parseInt(values.linesequencenumber),
-            item_display_name: <string>values['displayname.item'],
-            item_name: <string>values['itemid.item'],
-            location_name: <string>values.location?.text,
-            quantity: <number>parseFloat(values.quantity),
-            shipping_zip: <string>values.shipzip,
-        }),
-        table: 'InvoiceLine',
-        schema: [
-            { name: 'account', type: 'STRING' },
-            { name: 'amount', type: 'NUMERIC' },
-            { name: 'business_unit', type: 'STRING' },
-            { name: 'customer_id', type: 'STRING' },
-            { name: 'customer_industry', type: 'STRING' },
-            { name: 'customer_legacy_id', type: 'STRING' },
-            { name: 'customer_name', type: 'STRING' },
-            { name: 'customer_territory', type: 'STRING' },
-            { name: 'date', type: 'DATE' },
-            { name: 'document_number', type: 'STRING' },
-            { name: 'invoice_line', type: 'INT64' },
-            { name: 'item_display_name', type: 'STRING' },
-            { name: 'item_name', type: 'STRING' },
-            { name: 'location_name', type: 'STRING' },
-            { name: 'quantity', type: 'NUMERIC' },
-            { name: 'shipping_zip', type: 'STRING' },
-        ],
-    },
-    customdeploy_eag_ss_extract_1722: {
-        id: 1722,
-        parse: (values: any) => ({
-            id: <string>values.entityid,
-            name: <string>values.altname,
-            stage: <string>values.stage?.text,
-            email: <string>values.email,
-            phone: <string>values.phone,
-            business_unit: <string>values.custentity_ald_businessunit?.text,
-            legacy_id: values.custentity_ald_legacy_id,
-            terms: <string>values.terms?.text,
-            industry: <string>values.custentity_esc_industry?.text,
-            territory: <string>values.territory?.text,
-            salesrep: <string>values.salesrep?.text,
-            leadsource: <string>values.leadsource?.text,
-            datecreated: (() => {
-                const strValue = <string>values.datecreated;
-                const date = dayjs(strValue);
-                return date.isValid() ? date.toISOString() : null;
-            })(),
-            billing_address: <string>values.billaddress,
-            billing_state: <string>values.billstate?.text,
-            billing_zip: <string>values.billzipcode,
-        }),
-        table: 'Customer',
-        schema: [
-            { name: 'id', type: 'STRING' },
-            { name: 'name', type: 'STRING' },
-            { name: 'stage', type: 'STRING' },
-            { name: 'email', type: 'STRING' },
-            { name: 'phone', type: 'STRING' },
-            { name: 'business_unit', type: 'STRING' },
-            { name: 'legacy_id', type: 'STRING' },
-            { name: 'terms', type: 'STRING' },
-            { name: 'industry', type: 'STRING' },
-            { name: 'territory', type: 'STRING' },
-            { name: 'salesrep', type: 'STRING' },
-            { name: 'leadsource', type: 'STRING' },
-            { name: 'datecreated', type: 'TIMESTAMP' },
-            { name: 'billing_address', type: 'STRING' },
-            { name: 'billing_state', type: 'STRING' },
-            { name: 'billing_zip', type: 'STRING' },
-        ],
-    },
+const getConfig = () => {
+    const configs = {
+        customdeploy_eag_ss_extract_1772: {
+            id: 1772,
+            parse: (values: any) => ({
+                account: <string>values.account?.text,
+                amount: <number>parseFloat(values['amount']),
+                business_unit: <string>values.class?.text,
+                customer_id: <string>values['entityid.customer'],
+                customer_industry: <string>values['custentity_esc_industry.customer']?.text,
+                customer_legacy_id: <string>values['custentity_ald_legacy_id.customer'],
+                customer_name: <string>values['altname.customer'],
+                customer_territory: <string>values['territory.customer']?.text,
+                date: (() => {
+                    const strValue = <string>values.trandate;
+                    const date = dayjs(strValue);
+                    return date.isValid() ? date.format('YYYY-MM-DD') : null;
+                })(),
+                document_number: <string>values.tranid,
+                invoice_line: <number>parseInt(values.linesequencenumber),
+                item_display_name: <string>values['displayname.item'],
+                item_name: <string>values['itemid.item'],
+                location_name: <string>values.location?.text,
+                quantity: <number>parseFloat(values.quantity),
+                shipping_zip: <string>values.shipzip,
+            }),
+            table: 'InvoiceLine',
+            schema: [
+                { name: 'account', type: 'STRING' },
+                { name: 'amount', type: 'NUMERIC' },
+                { name: 'business_unit', type: 'STRING' },
+                { name: 'customer_id', type: 'STRING' },
+                { name: 'customer_industry', type: 'STRING' },
+                { name: 'customer_legacy_id', type: 'STRING' },
+                { name: 'customer_name', type: 'STRING' },
+                { name: 'customer_territory', type: 'STRING' },
+                { name: 'date', type: 'DATE' },
+                { name: 'document_number', type: 'STRING' },
+                { name: 'invoice_line', type: 'INT64' },
+                { name: 'item_display_name', type: 'STRING' },
+                { name: 'item_name', type: 'STRING' },
+                { name: 'location_name', type: 'STRING' },
+                { name: 'quantity', type: 'NUMERIC' },
+                { name: 'shipping_zip', type: 'STRING' },
+            ],
+        },
+        customdeploy_eag_ss_extract_1722: {
+            id: 1722,
+            parse: (values: any) => ({
+                id: <string>values.entityid,
+                name: <string>values.altname,
+                stage: <string>values.stage?.text,
+                email: <string>values.email,
+                phone: <string>values.phone,
+                business_unit: <string>values.custentity_ald_businessunit?.text,
+                legacy_id: values.custentity_ald_legacy_id,
+                terms: <string>values.terms?.text,
+                industry: <string>values.custentity_esc_industry?.text,
+                territory: <string>values.territory?.text,
+                salesrep: <string>values.salesrep?.text,
+                leadsource: <string>values.leadsource?.text,
+                datecreated: (() => {
+                    const strValue = <string>values.datecreated;
+                    const date = dayjs(strValue);
+                    return date.isValid() ? date.toISOString() : null;
+                })(),
+                billing_address: <string>values.billaddress,
+                billing_state: <string>values.billstate?.text,
+                billing_zip: <string>values.billzipcode,
+            }),
+            table: 'Customer',
+            schema: [
+                { name: 'id', type: 'STRING' },
+                { name: 'name', type: 'STRING' },
+                { name: 'stage', type: 'STRING' },
+                { name: 'email', type: 'STRING' },
+                { name: 'phone', type: 'STRING' },
+                { name: 'business_unit', type: 'STRING' },
+                { name: 'legacy_id', type: 'STRING' },
+                { name: 'terms', type: 'STRING' },
+                { name: 'industry', type: 'STRING' },
+                { name: 'territory', type: 'STRING' },
+                { name: 'salesrep', type: 'STRING' },
+                { name: 'leadsource', type: 'STRING' },
+                { name: 'datecreated', type: 'TIMESTAMP' },
+                { name: 'billing_address', type: 'STRING' },
+                { name: 'billing_state', type: 'STRING' },
+                { name: 'billing_zip', type: 'STRING' },
+            ],
+        },
+    };
+
+    const { deploymentId } = runtime.getCurrentScript();
+    const config = configs[deploymentId as keyof typeof configs];
+    if (!config) {
+        const _error = error.create({
+            name: 'CONFIG_ERROR',
+            message: `Deployment id ${deploymentId} not found in configs`,
+        });
+        log.error(_error.message, _error);
+        throw _error;
+    }
+    return config;
 };
 
 export const getInputData: EntryPoints.MapReduce.getInputData = () => {
-    const { deploymentId } = runtime.getCurrentScript();
-    return search.load({ id: configs[deploymentId as keyof typeof configs].id });
+    return search.load({ id: getConfig().id });
 };
 
 export const map: EntryPoints.MapReduce.map = (context) => {
-    const { deploymentId } = runtime.getCurrentScript();
     const { values } = JSON.parse(context.value);
     log.audit('values', values);
-    const row = mapValues(configs[deploymentId as keyof typeof configs].parse(values), (value) => value || null);
+    const row = mapValues(getConfig().parse(values), (value) => value || null);
     context.write('data', row);
 };
 
